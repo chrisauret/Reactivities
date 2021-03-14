a02fca72820829524e7679b55cb41620e0595639
chrisauret <chris.auret@yahoo.co.uk>
2021-03-14 21:56:09 +1100

    RegisterForm in modal

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Button, Container, Header, Segment, Image } from 'semantic-ui-react'
 import { RootStoreContext } from '../../app/stores/rootStore';
 import { LoginForm } from '../user/LoginForm';
+import { RegisterForm } from '../user/RegisterForm';
 
 export const HomePage = () => {
 
@@ -32,7 +33,7 @@ export const HomePage = () => {
                                 <Button onClick={() => openModal(<LoginForm />)} size='huge' inverted>
                                     Login
                                 </Button>
-                                <Button as={Link} to='/register' size='huge' inverted>
+                                <Button onClick={() => openModal(<RegisterForm />)} size='huge' inverted>
                                     Register
                             </Button>
                             </Fragment>
--- client-app/src/features/user/RegisterForm.tsx
+++ client-app/src/features/user/RegisterForm.tsx
@@ -2,12 +2,15 @@ import { FORM_ERROR } from 'final-form';
 import React, { useContext } from 'react'
 import { Form as FinalForm, Field } from 'react-final-form';
 import { combineValidators, isRequired } from 'revalidate';
-import { Form, Button, Label } from 'semantic-ui-react';
+import { Form, Button, Label, Header } from 'semantic-ui-react';
+import { ErrorMessage } from '../../app/common/form/ErrorMessage';
 import TextInput from '../../app/common/form/TextInput';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import { IUserFormValues } from '../../models/user';
 
 const validate = combineValidators({
+    username: isRequired('username'),
+    displayName: isRequired('display name'),
     email: isRequired('email'),
     password: isRequired('password')
 });
@@ -34,9 +37,21 @@ export const RegisterForm = () => {
                 pristine,
                 dirtySinceLastSubmit }) => (
                 <Form onSubmit={handleSubmit}>
+                    <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center' />
+                    <Field
+                        name='username'
+                        component={TextInput}
+                        placeholder='Username'
+                    />
+                    <Field
+                        name='displayName'
+                        component={TextInput}
+                        placeholder='Display Name'
+                    />
                     <Field
                         name='email'
                         component={TextInput}
+                        placeholder='Email'
                     />
                     <Field
                         name='password'
@@ -44,16 +59,19 @@ export const RegisterForm = () => {
                         placeholder='Password'
                         type='password'
                     />
-                    {submitError && !dirtySinceLastSubmit && <Label color='red' basic content={submitError.statusText} />}
-                    <br />
+                    {submitError && !dirtySinceLastSubmit && (
+                        <ErrorMessage
+                            error={submitError}
+                            text='Invalid email or password'
+                        />
+                    )}
                     <Button
                         disabled={invalid && !dirtySinceLastSubmit || pristine}
                         loading={submitting}
-                        positive
-                        content='Login' />
-                    <pre>
-                        {JSON.stringify(form.getState(), null, 2)}
-                    </pre>
+                        color='teal'
+                        content='Register'
+                        fluid
+                    />
                 </Form>
             )}
         />
