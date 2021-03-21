dc00d21be84f79c425f0203cea1ba32b9c2cd7cf
chrisauret <chris.auret@yahoo.co.uk>
2021-03-21 20:24:31 +1100

    Fix css import

--- client-app/src/app/common/photoUpload/PhotoWidgetCropper.tsx
+++ client-app/src/app/common/photoUpload/PhotoWidgetCropper.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import Cropper from 'react-cropper';
-import 'cropperjs/dist/cropper/css'
+import 'cropperjs/dist/cropper.css'
 
 interface IProps {
     setImage: (file: Blob) => void;
