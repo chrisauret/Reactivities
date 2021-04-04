ff5a186eae206d86d1874f769f3ca22bc2b3c79f
chrisauret <chris.auret@yahoo.co.uk>
2021-04-04 18:02:24 +1000

    Map comments

--- client-app/src/app/common/form/TextAreaInput.tsx
+++ client-app/src/app/common/form/TextAreaInput.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { FieldRenderProps } from 'react-final-form'
-import { Form, FormFieldProps, Label } from 'semantic-ui-react'
+import React from 'react';
+import { FieldRenderProps } from 'react-final-form';
+import { FormFieldProps, Form, Label } from 'semantic-ui-react';
 
-interface IProps extends FieldRenderProps<string, HTMLElement>, FormFieldProps { }
+interface IProps
+    extends FieldRenderProps<string, HTMLElement>,
+    FormFieldProps { }
 
-const TextInput: React.FC<IProps> = ({ input, width, rows, placeholder, meta: { touched, error } }) => {
+const TextAreaInput: React.FC<IProps> = ({
+    input,
+    width,
+    rows,
+    placeholder,
+    meta: { touched, error }
+}) => {
     return (
         <Form.Field error={touched && !!error} width={width}>
             <textarea rows={rows} {...input} placeholder={placeholder} />
@@ -14,7 +22,7 @@ const TextInput: React.FC<IProps> = ({ input, width, rows, placeholder, meta: { 
                 </Label>
             )}
         </Form.Field>
-    )
-}
+    );
+};
 
-export default TextInput;
+export default TextAreaInput;
