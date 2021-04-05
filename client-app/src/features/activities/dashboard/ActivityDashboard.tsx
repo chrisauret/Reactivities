1a1cbfbf7b48eca3af00b46e327eb2f6758421f7
chrisauret <chris.auret@yahoo.co.uk>
2021-04-05 17:06:57 +1000

    Add infinite scrolling

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,17 @@
 import { observer } from 'mobx-react-lite'
 import React, { useEffect, useState } from 'react'
 import { useContext } from 'react';
-import { Button, Grid } from 'semantic-ui-react'
+import { Grid, Loader } from 'semantic-ui-react'
 import { LoadingComponent } from '../../../app/layout/LoadingComponent';
 import ActivityList from './ActivityList'
 import { RootStoreContext } from '../../../app/stores/rootStore';
+import InfiniteScroll from 'react-infinite-scroller';
 
 const ActivityDashboard: React.FC = () => {
 
     const rootStore = useContext(RootStoreContext);
     const { loadActivities, loadingInitial, setPage, page, totalPages } = rootStore.activityStore;
-    const [loadingNext, setLoadingNext] = useState(false);
+    const [loadingNext, setLoadingNext] = useState(false); //565
 
     const handleGetNext = () => {
         setLoadingNext(true);
@@ -27,25 +28,31 @@ const ActivityDashboard: React.FC = () => {
     return (
         <Grid>
             <Grid.Column width={10}>
-                <ActivityList />
-                <Button
+                <InfiniteScroll //566
+                    pageStart={0}
+                    loadMore={handleGetNext}
+                    hasMore={!loadingNext && page + 1 < totalPages}
+                    initialLoad={false}
+                >
+                    <ActivityList />
+                </InfiniteScroll>
+                {/* <Button
                     floated='right'
                     content='more'
                     positive
                     disabled={totalPages === page + 1}
                     onClick={handleGetNext}
                     loading={loadingNext}
-                />
+                /> */}
             </Grid.Column>
             <Grid.Column width={6}>
                 <h2>Activity filters</h2>
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Loader active={loadingNext} />
             </Grid.Column>
         </Grid >
     );
 };
 
 export default observer(ActivityDashboard);
-
-function useSate(arg0: boolean): [any, any] {
-    throw new Error('Function not implemented.');
-}
